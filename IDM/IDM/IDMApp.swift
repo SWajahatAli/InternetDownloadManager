50d850f3d59d1f898e823c6bec40352d3acd9aed
SWajahatAli <swajahat.ali0@gmail.com>
2026-08-14 23:15:56 +0500

    Project Creation

---
+++ IDM/IDM/IDMApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDMApp.swift
+//  IDM
+//
+//  Created by Wajahat Ali on 14/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct IDMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IDM/IDMTests/IDMTests.swift
@@ -0,0 +1,17 @@
+//
+//  IDMTests.swift
+//  IDMTests
+//
+//  Created by Wajahat Ali on 14/08/2026.
+//
+
+import Testing
+@testable import IDM
+
+struct IDMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
